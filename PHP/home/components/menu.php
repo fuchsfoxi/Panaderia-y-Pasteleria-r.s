d81f72159667b6659c195233510c27d4f20c16bf
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-05-11 13:08:01 -0500

    Add files via upload

---
+++ PHP/home/components/menu.php
@@ -0,0 +1,8 @@
+
+    <nav>
+        <ul>
+            <li><a href="#">inicio</a></li>
+            <li><a href="#">Stock</a></li>
+            <li><a href="#">Historial</a></li>
+        </ul>
+    </nav>
