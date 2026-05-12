195d093647cb28c110bc193297e207be1bdd15c8
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-05-12 02:25:13 -0500

    dawda

--- app/core/Router.php
+++ app/core/Router.php
@@ -10,6 +10,14 @@ public function run():void {
     $partes = explode ('/', $url);
 
     $nombreController = !empty($partes[0])
-    
+    ? ucfirst(strtolower($partes[0])) . "Controller"
+    : "HomeController";
+
+    $metodo = !empty($partes[1]) ? $partes[1] : "index";
+
+    $sparams = array_slice($partes, 2);
+
+    $archivo = __DIR__ . '../controllers/' . $nombreController . '.php';
+
 }
 }
