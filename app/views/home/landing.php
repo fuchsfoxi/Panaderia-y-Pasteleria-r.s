b7dbf7a2a0108d00b0d385e050d0146222f3c234
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-05-14 01:54:11 -0500

    dawdad

---
+++ app/views/home/landing.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LANDING</title>
+</head>
+<body>
+    <div class="iniciar_Session_landig">
+        <a href="<?= BASE_URL ?>/login">Iniciar sesión</a>  
+    </div>
+    <div class="img_landing">
+        <img src="<?= BASE_URL ?>/public/img/imglanding.svg" alt="Imagen de panadería">
+    </div>
+
+    <div class="titulo_landing">
+        <h1>BIENVENIDO A LA PANADERIA Y PASTELERIA R.S</h1>
+    </div>
+
+    <div class="carta_general_landing">
+        <div class="carta_landing_1">
+            <img src="<?= BASE_URL ?>/public/icons/iconslanding1.svg" alt="icono">
+            <h2>Registra tu producción diaria</h2>
+            <p>Ingresa panes, bocaditos y tortas de cada turno de forma rápida y sencilla.</p>
+        </div>
+
+        <div class="carta_landing_2">
+            <img src="<?= BASE_URL ?>/public/icons/iconslanding2.svg" alt="icono">
+            <h2>Consulta tu historial</h2>
+            <p>Revisa y gestiona todos los registros de producción en cualquier momento.</p>
+        </div>
+    </div>
+</body> 
+</html>
