8d60e4fdff9d1a803884587a47215aaadc4bb925
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-05-13 20:17:35 -0500

    dwada

--- app/index.php
+++ app/index.php
@@ -1,56 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-</head>
-<body>
-    <?php include '../components/menu.php'; ?>
-    
-    <div class = "encabezado">
-        <h1>PANADERIA Y PASTELERIA R.S</h1>
-    </div>
+<?php
 
-    <div class = "cartas">
-        <ul>
-            <li>        
-                <div class = "contenido_carta">
-                    <p>Total de Panes</p>
-                    <div class="numero_total" id="totalPanes"></div>
-                </div>
-            </li>
+require_once __DIR__ . '/../config/Config.php';
+require_once __DIR__ . '/../core/App.php';
 
-            <li>
-                <div class = "contenido_carta">
-                    <p>Total de Pasteles</p>
-                    <div class="numero_total" id="totalPasteles"></div>
-                </div>
-            </li>
-
-            <li>
-                <div class="contenido_carta">
-                    <p>Total de Bocaditos</p>
-                    <div class="numero_total" id="totalBocaditos"></div>
-                </div>
-            </li>
-        </ul>
-    </div>
-
-    <div class="color_expl">
-        <ul>
-            <li class="color_ayer">
-                <p>Ayer</p>
-            </li>
-
-            <li class="color_hoy">
-                <p>Hoy</p>
-            </li>
-        </ul>
-    </div>
-
-    <div class="grafica">
-        <canvas id="grafica_comparativa"></canvas>
-    </div>
-</body>
-</html>
+$app = new App();
+$app->run();
