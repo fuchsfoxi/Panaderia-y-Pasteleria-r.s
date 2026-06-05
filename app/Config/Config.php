ce6a3f9a5ef56450bed69874e2d941456d85c50d
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-06-05 15:57:09 -0500

    dawd

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -14,9 +14,13 @@ if (file_exists($envFile)) {
 }
 
 
-define("DB_HOST", $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?? 'localhost');
-define("DB_PORT", $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?? '3306');
-define("DB_USER", $_ENV['DB_USERNAME'] ?? getenv('DB_USERNAME') ?? 'root');
-define("DB_NAME", $_ENV['DB_DATABASE'] ?? getenv('DB_DATABASE') ?? 'panaderia_rs');
-define("DB_PASSWORD", $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?? '');
-define("BASE_URL", $_ENV['APP_URL'] ?? getenv('APP_URL') ?? 'http://localhost/Panaderia-y-Pasteleria-r.s');
+define("DB_HOST", $_ENV['DB_HOST'] ?? 'localhost');
+define("DB_PORT", $_ENV['DB_PORT'] ?? '3306');
+define("DB_USER", $_ENV['DB_USERNAME'] ?? 'root');
+//temporalmente dejas el name de database y password vacios, modificar cuando se tenga tiempo
+define("DB_NAME", $_ENV['DB_DATABASE'] ?? 'panaderia_rs');
+define("DB_PASSWORD", $_ENV['DB_PASSWORD'] ?? '');
+
+
+//esto es temporal, modificar cuando se tenga tiempo 
+define("BASE_URL", $_ENV['APP_URL'] ?? 'http://localhost/Panaderia-y-Pasteleria-r.s');
