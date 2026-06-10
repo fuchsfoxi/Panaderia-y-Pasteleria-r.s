8302d09c638b83c704b4c9e2cb3a34821ffeecd7
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-06-09 20:44:03 -0500

    PHP MODELS USUARIOS
    se cambio las variables y se actualizo a la nueva versión de la base de datos

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -123,4 +123,86 @@ class Usuario {
                 ]);
             }
 
+            public function EliminarUsuario(int $id_usuario): bool {
+
+                $sql = "
+                    DELETE FROM usuario
+                    WHERE id_usuario = ?
+                ";
+
+                $stmt = $this->db->prepare($sql);
+
+                return $stmt->execute([$id_usuario]);
+                }
+
+            public function actualizarUsuario(
+                int $id_usuario,
+                string $nombre_usuario,
+                ?string $clave,
+                int $id_rol,
+                ?int $id_empleado
+            ): bool {
+
+                $sql = "
+                    UPDATE usuario
+                    SET 
+                        nombre_usuario = ?,
+                        id_rol = ?,
+                        id_empleado = ?
+                    WHERE id_usuario = ?
+                ";
+
+                $stmt = $this->db->prepare($sql);
+
+                $exito = $stmt->execute([
+                    $nombre_usuario,
+                    $id_rol,
+                    $id_empleado,
+                    $id_usuario
+                ]);
+
+                if ($exito && !empty($clave)) {
+                    return $this->actualizarClave($id_usuario, $clave);
+                }
+
+                return $exito;
+            }
+
+            public function obtenerPorId(int $id_usuario): array|false {
+
+            $sql = "
+                SELECT
+                    u.id_usuario,
+                    u.nombre_usuario,
+                    u.estado,
+                    u.id_rol,
+                    u.id_empleado
+                FROM usuario u
+                WHERE u.id_usuario = ?
+            ";
+
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute([$id_usuario]);
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+
+        public function actualizarClave(
+            int $id_usuario,
+            string $clave
+        ): bool {
+
+            $sql = "
+                UPDATE usuario
+                SET clave = ?
+                WHERE id_usuario = ?
+            ";
+
+            $stmt = $this->db->prepare($sql);
+
+            return $stmt->execute([
+                password_hash($clave, PASSWORD_DEFAULT),
+                $id_usuario
+            ]);
+        }
 }
